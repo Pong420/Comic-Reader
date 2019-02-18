d3113cf3735525ab64a62a6b9d50552771edacfa
Pong <samfunghp@gmail.com>
2019-02-18 11:01:15 +0800

    add theme & update dialog style

--- app/components/Loading/Loading.tsx
+++ app/components/Loading/Loading.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import createStyles from '@material-ui/core/styles/createStyles';
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Layout } from '../Layout';
 
-export interface LoadingProps extends WithStyles<typeof styles> {}
+export interface LoadingProps {}
 
-const styles = () =>
-  createStyles({
-    progress: {
-      color: '#fb0'
-    }
-  });
-
-export const Loading = withStyles(styles)(({ classes }: LoadingProps) => (
-  <Layout className="loading">
-    <CircularProgress className={classes.progress} size={50} />
-  </Layout>
-));
+export function Loading({  }: LoadingProps) {
+  return (
+    <Layout className="loading">
+      <CircularProgress size={50} color="secondary" />
+    </Layout>
+  );
+}
--- app/containers/Root.tsx
+++ app/containers/Root.tsx
@@ -2,8 +2,20 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import Routes from '../Routes';
 import { History } from 'history';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import Routes from '../Routes';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2a2a2a'
+    },
+    secondary: {
+      main: '#fb0'
+    }
+  }
+});
 
 type Props = {
   store: any;
@@ -14,11 +26,13 @@ export default class Root extends Component<Props> {
   render() {
     const { store, history } = this.props;
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Routes />
+          </ConnectedRouter>
+        </Provider>
+      </MuiThemeProvider>
     );
   }
 }
