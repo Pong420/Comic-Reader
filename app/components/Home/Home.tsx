3c5509bc26a22c606dd670ed0edc26c98369a74a
Pong <samfunghp@gmail.com>
2019-02-21 13:35:41 +0800

    remove home header

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -3,7 +3,6 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { AutoSizer } from 'react-virtualized';
 import { Layout } from '../Layout';
-import { HomeHeader } from './HomeHeader';
 import { GridContainer } from '../GridContainer';
 import { Grid } from '../Grid';
 import { getLatestUpdate } from '../../api';
@@ -31,8 +30,6 @@ export const Home = connect(
   mapStateToProps,
   mapDispatchToProps
 )(function({ page, comicList, addComics }: HomeProps) {
-  const tabs = ['最新更新', '收藏'];
-
   const loadMoreLatestComic = () => {
     const nextPage = page + 1;
     const from = comicList.length;
@@ -55,20 +52,17 @@ export const Home = connect(
 
   return (
     <Layout className="home">
-      <HomeHeader tabs={tabs} onChange={label => {}} />
-      <div className="home-grids">
-        <AutoSizer>
-          {({ width, height }) => (
-            <GridContainer
-              width={width}
-              height={height}
-              list={comicList}
-              onGridRender={props => <Grid {...props} />}
-              loadMore={loadMoreLatestComic}
-            />
-          )}
-        </AutoSizer>
-      </div>
+      <AutoSizer>
+        {({ width, height }) => (
+          <GridContainer
+            width={width}
+            height={height}
+            list={comicList}
+            onGridRender={props => <Grid {...props} />}
+            loadMore={loadMoreLatestComic}
+          />
+        )}
+      </AutoSizer>
     </Layout>
   );
 });
