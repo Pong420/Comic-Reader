76f660067ed5f8b47cd0bc7683bb7a3c13b6571d
Pong <samfunghp@gmail.com>
2019-02-24 21:53:29 +0800

    remove react hot loader

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import { configureStore, history } from './store/configureStore';
 import Root from './containers/Root';
 import './app.global.scss';
@@ -8,21 +7,10 @@ import './app.global.scss';
 const store = configureStore();
 
 render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
+  <Root store={store} history={history} />,
   document.getElementById('root')
 );
 
 if ((module as any).hot) {
-  (module as any).hot.accept('./containers/Root', () => {
-    // eslint-disable-next-line global-require
-    const NextRoot = require('./containers/Root').default;
-    render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+  (module as any).hot.accept();
 }
--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -53,7 +53,6 @@ export default merge.smart(baseConfig, {
   target: 'electron-renderer',
 
   entry: [
-    'react-hot-loader/patch',
     `webpack-dev-server/client?http://localhost:${port}/`,
     'webpack/hot/only-dev-server',
     require.resolve('../app/index.tsx')
