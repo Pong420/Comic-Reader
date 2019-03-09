08ca0c30a1d9f4d51a29b537ac6fb16ea07002d9
Pong <samfunghp@gmail.com>
2019-03-09 19:31:59 +0800

    fix image navigation

--- app/components/Content/Images/Images.tsx
+++ app/components/Content/Images/Images.tsx
@@ -1,15 +1,7 @@
-import React, { KeyboardEvent, useEffect } from 'react';
+import React, { KeyboardEvent, useEffect, useRef } from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
-import {
-  RootState,
-  ImagesState,
-  ImageActionCreators
-  // ImageActions
-} from '../../../store';
-
-// TODO:
-// - Error Handling
+import { RootState, ImagesState, ImageActionCreators } from '../../../store';
 
 export interface ImageProsp {
   activeIndex: number;
@@ -31,8 +23,12 @@ export function ImagesComponent({
   preloadImage,
   stopPreloadImage
 }: ImageProsp & ImagesState & typeof ImageActionCreators) {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (imagesDetail[activeIndex]) {
+      scrollRef.current!.scrollTop = 0;
+
       preloadImage(imagesDetail, activeIndex);
 
       return () => {
@@ -42,10 +38,10 @@ export function ImagesComponent({
   }, [imagesDetail.length, activeIndex]);
 
   return (
-    <div className="images" tabIndex={0} onKeyDown={onKeyDown}>
+    <div className="images" tabIndex={0} onKeyDown={onKeyDown} ref={scrollRef}>
       <div className="image-loading">撈緊...</div>
-      {imagesDetail.map((detail, index) => {
-        const { src, loaded, error } = detail;
+      {imagesDetail.map(detail => {
+        const { src, loaded, error, index } = detail;
         const hidden = index !== activeIndex;
         const imgSrc = loaded ? src : '';
 
@@ -53,19 +49,7 @@ export function ImagesComponent({
           return <div className="image-error">張圖撈唔到，試下下一頁</div>;
         }
 
-        return (
-          <img
-            key={index}
-            src={imgSrc}
-            hidden={hidden}
-            // onLoad={() => {
-            //   loadImageEnded({ ...detail, loaded: true });
-            // }}
-            // onError={() => {
-            //   loadImageEnded({ ...detail, error: true });
-            // }}
-          />
-        );
+        return <img key={index} src={imgSrc} hidden={hidden} />;
       })}
     </div>
   );
