c5572bb7923666a11a8be36bbbbc9540dc6503e9
Pong <samfunghp@gmail.com>
2019-03-10 23:37:34 +0800

    add bookmark btn in content sidebar

--- app/components/Content/ContentSidebar/ContentSidebar.tsx
+++ app/components/Content/ContentSidebar/ContentSidebar.tsx
@@ -4,6 +4,7 @@ import { RouteComponentProps } from 'react-router';
 import Previous from '@material-ui/icons/ArrowBack';
 import { Sidebar, SidebarIcon } from '../../Sidebar';
 import { PageNoButton } from '../PageNoButton';
+import { BookmarkBtn } from '../../BookmarkBtn';
 import { RootState, ContentState } from '../../../store';
 
 interface MatchParams {
@@ -12,9 +13,7 @@ interface MatchParams {
 }
 
 function mapStateToProps({ content }: RootState) {
-  return {
-    ...content
-  };
+  return { ...content };
 }
 
 const FlexSpacer = () => <div style={{ flex: '1 1 auto' }} />;
@@ -32,6 +31,7 @@ export const ContentSidebar = connect(mapStateToProps)(
           tooltip="返回章節"
           onClick={() => history.push(`/comic/${match.params.comicID}`)}
         />
+        <BookmarkBtn comicID={match.params.comicID} />
         <SidebarIcon Component={FlexSpacer} />
         <SidebarIcon
           Component={PageNoButton}
