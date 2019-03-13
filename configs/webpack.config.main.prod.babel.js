eca77a9579db21bfca3674378255f0a8c8adc8e1
Pong <samfunghp@gmail.com>
2019-03-13 20:47:03 +0800

    remove test pkg

--- configs/webpack.config.main.prod.babel.js
+++ configs/webpack.config.main.prod.babel.js
@@ -5,7 +5,6 @@
 import path from 'path';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
-import TerserPlugin from 'terser-webpack-plugin';
 import baseConfig from './webpack.config.base';
 import CheckNodeEnv from '../internals/scripts/CheckNodeEnv';
 
@@ -23,18 +22,6 @@ export default merge.smart(baseConfig, {
   output: {
     path: path.join(__dirname, '..'),
     filename: './app/main.prod.js'
-  },
-
-  optimization: {
-    minimizer: process.env.E2E_BUILD
-      ? []
-      : [
-          new TerserPlugin({
-            parallel: true,
-            sourceMap: true,
-            cache: true
-          })
-        ]
   },
 
   plugins: [
--- internals/scripts/CreateComponent.js
+++ internals/scripts/CreateComponent.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const name = process.argv[2].replace(/^\w/, function(chr) {
+const name = process.argv[2].replace(/^\w/, chr => {
   return chr.toUpperCase();
 });
 const dir = `./app/components/${name}`;
