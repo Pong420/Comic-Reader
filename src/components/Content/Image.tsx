5f4853485b7bd0ca74db51a11eaabf551a0e6296
Pong <samfunghp@gmail.com>
2019-06-11 23:04:13 +0800

    fix image

--- src/components/Content/Image.tsx
+++ src/components/Content/Image.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useMemo, CSSProperties } from 'react';
 import { Schema$ImageDetail } from '../../typings';
 
 interface Props extends Schema$ImageDetail {
   hidden?: boolean;
 }
 
-export function Image({ src, loaded, error, hidden }: Props) {
+export function Image({ src, loaded, error, width, height, hidden }: Props) {
+  const style = useMemo<CSSProperties>(() => ({ width, height }), [
+    width,
+    height
+  ]);
+
   return (
-    <div className="image" hidden={hidden}>
+    <div className="image" hidden={hidden} style={style}>
       {error ? 'FAIL' : loaded ? <img src={src} alt={src} /> : 'LOADING...'}
     </div>
   );
